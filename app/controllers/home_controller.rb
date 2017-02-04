78908165b99071485a22ca306c4b1d1f65317d3c
Ziserman <martinziserman@gmail.com>
2017-02-04 21:51:45 +0100

    assets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,16 @@
 class HomeController < ApplicationController
   def index
     if current_user.blank?
-      if cookies[:user_id].blank?
+      if cookies.signed[:user_id].blank?
         iu = InvitedUser.create
         cookies.signed[:user_id] = iu.id
       else
-
+        iu = InvitedUser.find(cookies.signed[:user_id])
+        redirect_to(:controller => "game", :action => "index") unless iu.team.blank?
       end
     else
       cookies.signed[:user_id] = current_user.id
+      redirect_to(:controller => "game", :action => "index") unless current_user.team.blank?
     end
     @teams = Team.all
   end
--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -3,18 +3,18 @@ class TeamController < ApplicationController
     if user_signed_in?
       current_user.team = Team.find(params[:id])
       current_user.save
+      ActionCable.server.remote_connections.where(current_user: User
+        .find(cookies.signed[:user_id])).disconnect
     elsif !cookies.signed[:user_id].blank?
       iu = InvitedUser.find(cookies.signed[:user_id])
       iu.team = Team.find(params[:id])
       iu.save
+      ActionCable.server.remote_connections.where(current_user: InvitedUser
+        .find(cookies.signed[:user_id])).disconnect
     else
       iu = InvitedUser.create(team_id: params[:id])
     end
-    # redirect_to :controller => "game", :action => "index"
 
-    redirect_to action: "tampon"
-  end
-  def tampon
     redirect_to :controller => "game", :action => "index"
   end
 end
