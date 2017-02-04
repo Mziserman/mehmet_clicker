ff58125c3d593faeae6feb88f04c9d31a77ddbde
Ziserman <martinziserman@gmail.com>
2017-02-04 23:21:11 +0100

    assets

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -3,18 +3,22 @@ class TeamController < ApplicationController
     if user_signed_in?
       current_user.team = Team.find(params[:id])
       current_user.save
-      ActionCable.server.remote_connections.where(current_user: User
-        .find(cookies.signed[:user_id])).disconnect
+      refresh_connection(current_user)
+
     elsif !cookies.signed[:user_id].blank?
       iu = InvitedUser.find(cookies.signed[:user_id])
       iu.team = Team.find(params[:id])
       iu.save
-      ActionCable.server.remote_connections.where(current_user: InvitedUser
-        .find(cookies.signed[:user_id])).disconnect
+      refresh_connection(iu)
     else
       iu = InvitedUser.create(team_id: params[:id])
     end
 
     redirect_to :controller => "game", :action => "index"
+  end
+
+  def refresh_connection(user)
+    ActionCable.server.remote_connections.where(current_user: user).disconnect
+    ActionCable.server.add_connection(current_user: user)
   end
 end
