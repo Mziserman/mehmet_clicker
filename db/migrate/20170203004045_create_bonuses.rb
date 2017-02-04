ba712be5f822d3b8c15f331775269486d33a1338
Ziserman <martinziserman@gmail.com>
2017-02-04 18:57:03 +0100

    assets

--- db/migrate/20170203004045_create_bonuses.rb
+++ db/migrate/20170203004045_create_bonuses.rb
@@ -3,9 +3,9 @@ class CreateBonuses < ActiveRecord::Migration[5.0]
     create_table :bonuses do |t|
       t.string :name
       t.integer :base_click_bonus, default: 1
-      t.float :level_up_click_inc, default: 1
+      t.float :level_up_click_inc, default: 0.3
       t.integer :base_price, default: 1
-      t.float :level_up_price_inc, default: 1
+      t.float :level_up_price_inc, default: 0.6
 
       t.timestamps
     end
--- db/migrate/20170203115913_create_auto_clicker_bonuses.rb
+++ db/migrate/20170203115913_create_auto_clicker_bonuses.rb
@@ -4,8 +4,8 @@ class CreateAutoClickerBonuses < ActiveRecord::Migration[5.0]
       t.string :name
       t.integer :base_click_bonus, default: 1
       t.integer :base_price, default: 1
-      t.float :level_up_click_inc, default: 1
-      t.float :level_up_price_inc, default: 1
+      t.float :level_up_click_inc, default: 0.3
+      t.float :level_up_price_inc, default: 0.6
 
       t.timestamps
     end
