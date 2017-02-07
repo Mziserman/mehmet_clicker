5eead3fe1179fe0e520c52e2c2d9634cda07adfe
Tom Bonnike <bonniketom@gmail.com>
2017-02-07 17:06:32 +0100

    Added homepage styles & layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get 'trailer', to: 'home#trailer'
-  get 'synopsis', to: 'home#synopsis'
-  get 'gallerie', to: 'home#gallery'
-  get 'equipe', to: 'home#team'
+  get 'trailer', to: 'home#trailer', as: 'trailer'
+  get 'synopsis', to: 'home#synopsis', as: 'synopsis'
+  get 'gallerie', to: 'home#gallery', as: 'gallery'
+  get 'equipe', to: 'home#team', as: 'team'
 
   devise_for :users
 
-  get 'jeu', to: 'game#index'
+  get 'jeu', to: 'game#index', as: 'game'
   get 'jeu/jouer', to: 'game#game'
 
   get 'team/index'
