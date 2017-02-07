8ff49c272665bbbd99331ca042013743198ac11f
Ziserman <martinziserman@gmail.com>
2017-02-07 18:36:19 +0100

    fix game

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,11 +8,9 @@ class GameController < ApplicationController
         cookies.signed[:user_id] = iu.id
       else
         iu = InvitedUser.find(cookies.signed[:user_id])
-        redirect_to(:controller => "game", :action => "index") unless iu.team.blank?
       end
     else
       cookies.signed[:user_id] = current_user.id
-      redirect_to(:controller => "game", :action => "index") unless current_user.team.blank?
     end
     @teams = Team.all
     @goals = Goal.all
