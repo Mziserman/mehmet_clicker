cb581c77d9f743becd7695751249f7cb5bb04de2
Ziserman <martinziserman@gmail.com>
2017-02-07 18:34:06 +0100

    fix game

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,11 +2,25 @@ class GameController < ApplicationController
   layout 'game'
 
   def index
-    @user = user_signed_in? ? current_user : InvitedUser.find(cookies.signed[:user_id])
-    @team = @user.team
+    if current_user.blank?
+      if cookies.signed[:user_id].blank?
+        iu = InvitedUser.create
+        cookies.signed[:user_id] = iu.id
+      else
+        iu = InvitedUser.find(cookies.signed[:user_id])
+        redirect_to(:controller => "game", :action => "index") unless iu.team.blank?
+      end
+    else
+      cookies.signed[:user_id] = current_user.id
+      redirect_to(:controller => "game", :action => "index") unless current_user.team.blank?
+    end
     @teams = Team.all
+    @goals = Goal.all
   end
 
   def game
+    @user = user_signed_in? ? current_user : InvitedUser.find(cookies.signed[:user_id])
+    @team = @user.team
+    @teams = Team.all
   end
 end
